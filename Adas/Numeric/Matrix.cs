ce137ac78323cc0aa9ea5916223f08e0975389a8
Mito Kehayov <Mitokehayov@gmail.com>
2019-02-14 19:53:27 +0100

    matrix structure init

---
+++ Adas/Numeric/Matrix.cs
@@ -0,0 +1,24 @@
+﻿namespace Adas.Numeric
+{
+    public class Matrix<T>
+    {
+        private readonly T[,] _matrixElements;
+
+        public Matrix(int rows, int cols)
+        {
+            _matrixElements = new T[rows, cols];
+        }
+
+        public Matrix(T[,] elements)
+        {
+            _matrixElements = elements;
+        }
+
+        public T Elements {
+            get
+            {
+                return _matrixElements;
+            }
+        }
+    }
+}
